c7017147117a76cbc54189e634547298c002ae17
sampathNG <jnanisampath@gmail.com>
2023-12-11 18:53:18 +0530

    bg-toggler done

--- day1/src/App.jsx
+++ day1/src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("blue")
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full h-screen flex justify-center" style={{ backgroundColor:color}}>
+    <div className="bg-black h-24 w-48 flex justify-center items-center self-end mb-20 rounded-2xl">
+    <button className="bg-yellow-400 h-8 mr-4 rounded-md p-4 text-center flex items-center" onClick={()=>setColor("yellow")}>Yellow</button>
+    <button className="bg-red-400 h-6 rounded-md p-4 flex items-center" onClick={()=>setColor("red")}>Red</button>
+    </div>
+    </div>
   )
 }
 
